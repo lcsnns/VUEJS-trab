0f331cf2866a265de3d455a4aa94b962782ffac9
lucas nunes <145053610+lcsnns@users.noreply.github.com>
2024-05-07 17:45:28 -0300

    COMMITINICUAL

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div>
+      <input type="number" v-model="numero1" @input="calcularResultado">
+      <select v-model="operacao" @change="calcularResultado">
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">*</option>
+        <option value="/">/</option>
+      </select>
+      <input type="number" v-model="numero2" @input="calcularResultado">
+    </div>
+    <div>Resultado: {{ resultado }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      numero1: 0,
+      numero2: 0,
+      operacao: '+',
+      resultado: 0
+    };
+  },
+  methods: {
+    calcularResultado() {
+      switch (this.operacao) {
+        case '+':
+          this.resultado = parseFloat(this.numero1) + parseFloat(this.numero2);
+          break;
+        case '-':
+          this.resultado = parseFloat(this.numero1) - parseFloat(this.numero2);
+          break;
+        case '*':
+          this.resultado = parseFloat(this.numero1) * parseFloat(this.numero2);
+          break;
+        case '/':
+          this.resultado = parseFloat(this.numero1) / parseFloat(this.numero2);
+          break;
+        default:
+          this.resultado = 0;
+      }
+    }
+  }
+};
+</script>
+
+<style>
+/* Estilos CSS aqui */
+</style>
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'; // Importe createApp do Vue
+import App from './App.vue';
+
+createApp(App).mount('#app');
